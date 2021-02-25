6aa29fe6d731586b4932590c3311081bf3db735c
Hubert Rusinek <rusinekh@gmail.com>
2021-02-25 16:31:52 +0100

    JDP210201-5 Fixed build.gradle and UserDto

--- src/main/java/com/kodilla/ecommercee/user/domain/UserDto.java
+++ src/main/java/com/kodilla/ecommercee/user/domain/UserDto.java
@@ -1,32 +1,13 @@
 package com.kodilla.ecommercee.user.domain;
 
-public class UserDto {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
+@AllArgsConstructor
+public class UserDto {
     private Long id;
     private String userName;
-    private int status;
+    private Integer status;
     private Long userKey;
-
-    public UserDto(Long id, String userName, int status, Long userKey) {
-        this.id = id;
-        this.userName = userName;
-        this.status = status;
-        this.userKey = userKey;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public int getStatus() {
-        return status;
-    }
-
-    public Long getUserKey() {
-        return userKey;
-    }
 }
