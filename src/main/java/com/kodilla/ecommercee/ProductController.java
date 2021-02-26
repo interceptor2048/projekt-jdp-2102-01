4cf1f5e9143940761ec0c2f3b53b207e172d8baa
Bauaser <michal.bala.1989@gmail.com>
2021-02-26 22:57:55 +0100

    JDP210201-1 ProductController initial implementation

--- src/main/java/com/kodilla/ecommercee/ProductController.java
+++ src/main/java/com/kodilla/ecommercee/ProductController.java
@@ -1,4 +1,38 @@
 package com.kodilla.ecommercee;
 
+import com.kodilla.ecommercee.domain.ProductDto;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/v1/product")
 public class ProductController {
+    @GetMapping(value = "getProducts")
+    public List<ProductDto> getProducts() {
+        return new ArrayList<>();
+    }
+
+    @GetMapping(value = "getProduct")
+    public ProductDto getProduct (@RequestParam int productId) {
+        return new ProductDto(1, "sample product", "sample description", 2.5, 1);
+    }
+
+    @PostMapping(value = "createProduct")
+    public void createProduct(@RequestBody ProductDto productDto) {
+
+    }
+
+    @PutMapping(value = "updateProduct")
+    public ProductDto updateProduct(@RequestBody ProductDto productDto) {
+        return new ProductDto (1, "edited sample product", "edited sample description", 2.5, 1);
+    }
+
+    @DeleteMapping(value = "deleteProduct")
+    public void deleteProduct() {
+
+    }
 }
--- src/main/java/com/kodilla/ecommercee/domain/ProductDto.java
+++ src/main/java/com/kodilla/ecommercee/domain/ProductDto.java
@@ -1,2 +1,16 @@
-package com.kodilla.ecommercee.domain;public class ProductDto {
+package com.kodilla.ecommercee.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductDto {
+    private int id;
+    private String productName;
+    private String description;
+    private double price;
+    private int groupId;
 }
