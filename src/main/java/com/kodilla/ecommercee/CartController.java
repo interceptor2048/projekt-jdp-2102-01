160aa14e06b08d9e7720ec75dd1403b529b07c95
Mateusz <mateusz@com>
2021-02-26 21:30:51 +0100

    CartController request method added. CartDto fix.

--- src/main/java/com/kodilla/ecommercee/CartController.java
+++ src/main/java/com/kodilla/ecommercee/CartController.java
@@ -1,4 +1,39 @@
 package com.kodilla.ecommercee;
 
+import com.kodilla.ecommercee.domain.CartDto;
+import com.kodilla.ecommercee.domain.OrderDto;
+import com.kodilla.ecommercee.domain.ProductDto;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/cart")
 public class CartController {
+
+    @RequestMapping(method = RequestMethod.POST, value = "createNewCart")
+    public CartDto createNewCart() {
+        return new CartDto();
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "getProducts")
+    public List<ProductDto> getProducts() {
+        return new ArrayList<>();
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "addToCart")
+    public void addProductsToCart(@RequestParam Long productId) {
+        new CartDto(1L, productId, 1L);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "deleteProduct")
+    public void deleteProduct(@RequestParam Long productId) {
+
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "createOrder")
+    public OrderDto createOrder(@RequestBody CartDto cartDto) {
+        return new OrderDto(cartDto.getId(), cartDto.getProductId(), cartDto.getUserId());
+    }
 }
--- src/main/java/com/kodilla/ecommercee/domain/OrderDto.java
+++ src/main/java/com/kodilla/ecommercee/domain/OrderDto.java
@@ -2,14 +2,12 @@ package com.kodilla.ecommercee.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
-@NoArgsConstructor
-public class CartDto {
+public class OrderDto {
 
     private Long id;
-    private Long userId;
+    private Long cartId;
     private Long productId;
 }
