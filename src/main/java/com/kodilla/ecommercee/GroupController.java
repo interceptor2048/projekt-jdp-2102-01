132639a2dc925212d8ee71848b90fa8a37b828d4
lukkra1989 <luk.kramarczyk@gmail.com>
2021-02-27 17:51:55 +0100

    JDP210201-2 Created GroupController

--- src/main/java/com/kodilla/ecommercee/GroupController.java
+++ src/main/java/com/kodilla/ecommercee/GroupController.java
@@ -1,4 +1,33 @@
 package com.kodilla.ecommercee;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/api/groups")
 public class GroupController {
+
+    @GetMapping
+    public List<GroupDto> getGroups() {
+        return new ArrayList<>();
+    }
+
+    @PostMapping
+    public void createGroup(@RequestBody GroupDto groupDto) {
+    }
+
+    @GetMapping("/{groupId}")
+    public GroupDto getGroup(@PathVariable int groupId) {
+        return new GroupDto(123, "First Group");
+    }
+
+    @PutMapping
+    public GroupDto updateGroup(@RequestBody GroupDto groupDto) {
+        return new GroupDto(123, "Test content");
+    }
+
 }
---
+++ src/main/java/com/kodilla/ecommercee/GroupDto.java
@@ -0,0 +1,27 @@
+package com.kodilla.ecommercee;
+
+public class GroupDto {
+    private int id;
+    private String name;
+
+    public GroupDto(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
