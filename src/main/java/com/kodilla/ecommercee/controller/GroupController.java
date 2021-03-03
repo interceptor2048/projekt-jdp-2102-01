78488af1781df320f62d75817eebf06737a6928f
Hubert Rusinek <rusinekh@gmail.com>
2021-03-03 17:18:02 +0100

    Fixed GroupController

--- src/main/java/com/kodilla/ecommercee/controller/GroupController.java
+++ src/main/java/com/kodilla/ecommercee/controller/GroupController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
-@RestController
+@RestController("GroupController")
 @RequestMapping("/v1/group")
 public class GroupController {
 
