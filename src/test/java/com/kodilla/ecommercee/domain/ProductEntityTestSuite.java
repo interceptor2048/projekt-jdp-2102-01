3f331428abaf9b76f6b997d42e3505cd091ea5bd
Mateusz <mateusz@com>
2021-03-04 19:14:53 +0100

    added empty testProduct.

--- src/test/java/com/kodilla/ecommercee/domain/ProductEntityTestSuite.java
+++ src/test/java/com/kodilla/ecommercee/domain/ProductEntityTestSuite.java
@@ -1,4 +1,27 @@
 package com.kodilla.ecommercee.domain;
 
+import com.kodilla.ecommercee.repository.UserRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+@RunWith(SpringRunner.class)
 public class ProductEntityTestSuite {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void testProductEntity() {
+        //Given
+
+        //When
+
+        //Then
+    }
 }
