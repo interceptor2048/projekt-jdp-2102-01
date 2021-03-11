3b4d379929dcfb8095e389db2760977dea559367
Mateusz <mateusz@com>
2021-03-11 19:06:39 +0100

    Product Entity testDeleteProduct added

--- src/test/java/com/kodilla/ecommercee/domain/ProductEntityTestSuite.java
+++ src/test/java/com/kodilla/ecommercee/domain/ProductEntityTestSuite.java
@@ -1,6 +1,8 @@
 package com.kodilla.ecommercee.domain;
 
-import com.kodilla.ecommercee.repository.ProductRepository;
+import com.kodilla.ecommercee.controller.exceptions.ProductConflictException;
+import com.kodilla.ecommercee.service.GroupDbService;
+import com.kodilla.ecommercee.service.ProductDbService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,13 +10,21 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import static org.junit.Assert.*;
+
 @SpringBootTest
 @Transactional
 @RunWith(SpringRunner.class)
 public class ProductEntityTestSuite {
 
     @Autowired
-    private ProductRepository productRepository;
+    private ProductDbService productDbService;
+
+    @Autowired
+    private GroupDbService groupDbService;
 
     @Test
     public void testProductEntity() {
@@ -26,7 +36,7 @@ public class ProductEntityTestSuite {
     }
 
     @Test
-    public void testProductSave() {
+    public void testSaveProduct() {
         //Given
 
         //When
@@ -35,7 +45,7 @@ public class ProductEntityTestSuite {
     }
 
     @Test
-    public void testProductUpdate() {
+    public void testUpdateProduct() {
         //Given
 
         //When
@@ -44,11 +54,23 @@ public class ProductEntityTestSuite {
     }
 
     @Test
-    public void testProductDelete() {
+    public void testDeleteProduct() {
         //Given
+        Product product = new Product(1L, "product 1", "content 1", 13.00);
+
+        try {
+            productDbService.saveProduct(product);
+        } catch (ProductConflictException e) {
+            e.printStackTrace();
+        }
+
+        Long productId = product.getId();
 
         //When
+        productDbService.deleteProduct(productId);
+        Optional<Product> productOptional = productDbService.getProductById(productId);
 
         //Then
+        assertFalse(productOptional.isPresent());
     }
 }
