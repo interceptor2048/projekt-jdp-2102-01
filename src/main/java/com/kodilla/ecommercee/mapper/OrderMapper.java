41e901b4ab85a48d065ac1feb70e74ad59922ef7
Pawel <pawel.tomczyk85@gmail.com>
2021-03-15 13:25:39 +0100

    JDP210201-24 OrderMapper modified

--- src/main/java/com/kodilla/ecommercee/mapper/OrderMapper.java
+++ src/main/java/com/kodilla/ecommercee/mapper/OrderMapper.java
@@ -5,23 +5,38 @@ import com.kodilla.ecommercee.domain.dto.OrderDto;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class OrderMapper {
     public Order mapToOrder(final OrderDto orderDto) {
-        return new Order();
-
+        return new Order(
+                orderDto.getId(),
+                orderDto.getUserId(),
+                orderDto.isVerified(),
+                orderDto.isSent(),
+                orderDto.isPaid()
+        );
     }
 
     public OrderDto mapToOrderDto(final Order order) {
-        return new OrderDto();
-
+        return new OrderDto(
+                order.getId(),
+                order.getUser(),
+                order.isVerified(),
+                order.isSent(),
+                order.isPaid()
+        );
     }
 
-    public List<Order> orderDtoListToOrder(List<OrderDto> orderDtoList) {
-        return new ArrayList<>();
+    public List<Order> orderDtoListMapToOrderList(List<OrderDto> orderDtoList) {
+        return orderDtoList.stream()
+                .map(this::mapToOrder)
+                .collect(Collectors.toList());
     }
 
-    public List<OrderDto> orderListToOrderTo(List<Order> orderList) {
-        return new ArrayList<>();
+    public List<OrderDto> orderListMapToOrderDtoList(List<Order> orderList) {
+        return orderList.stream()
+                .map(this::mapToOrderDto)
+                .collect(Collectors.toList());
     }
 }
