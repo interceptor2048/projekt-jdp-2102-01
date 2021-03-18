727d35b9bd183e079cb0294a3cf4a0af09bd15c6
Bauaser <michal.bala.1989@gmail.com>
2021-03-18 17:56:00 +0100

    update proposal submitted

--- src/main/java/com/kodilla/ecommercee/processor/OrderProcessor.java
+++ src/main/java/com/kodilla/ecommercee/processor/OrderProcessor.java
@@ -1,8 +1,11 @@
 package com.kodilla.ecommercee.processor;
 
+import com.kodilla.ecommercee.controller.OrderController;
 import com.kodilla.ecommercee.domain.Order;
 import com.kodilla.ecommercee.domain.User;
+import com.kodilla.ecommercee.mapper.OrderMapper;
 import com.kodilla.ecommercee.repository.UserRepository;
+import com.kodilla.ecommercee.service.information.EmailSender;
 
 import java.util.List;
 
@@ -11,24 +14,47 @@ public class OrderProcessor implements Observable {
     private List<User> users;
     UserRepository userRepository;
 
-    private Order order;
+    //private Order order;
 
+    private OrderController controller;
 
-    public OrderProcessor(Order order) {
+    private OrderMapper mapper;
+
+    private EmailSender emailSender;
+
+    /*public OrderProcessor(Order order) {
         this.order = order;
 
+    }*/
+
+    public void verifyOrder (Order order) {
+        order.setVerified(true);
+        controller.updateOrder(mapper.mapToOrderDto(order));
+        emailSender.notifyOrderVerified(order);
+    }
+
+    public void payOrder (Order order) {
+        order.setPaid(true);
+        controller.updateOrder(mapper.mapToOrderDto(order));
+        emailSender.notifyOrderPaid(order);
+    }
+
+    public void sendOrder (Order order) {
+        order.setSent(true);
+        controller.updateOrder(mapper.mapToOrderDto(order));
+        emailSender.notifyOrderSent(order);
     }
 
     @Override
     public void notifyUsers() {
-        if (order.isVerified()) {
+       /* if (order.isVerified()) {
             order.setPaid(true);
 
             System.out.println("Order status changed into: " + order.isVerified() + order.isPaid());
         } else {
             order.setPaid(false);
             System.out.println("Order status: " + order.isVerified() + order.isPaid());
-        }
+        }*/
 
     }
 
